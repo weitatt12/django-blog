cd2521f408054e2c6b0d04e90c273db815975e17
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-09-15 12:27:43 +0800

    added database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'CorneyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post conetnt',
-        'date_posted': 'Aug 27, 2018'
-    },
-    {
-        'author': 'Johnny',
-        'title': 'Blog Post 2',
-        'content': 'Second post conetnt',
-        'date_posted': 'Aug 28, 2017'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
